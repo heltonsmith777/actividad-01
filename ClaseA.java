b96c9c5f459e333b5ab74bd3238f4f48dbdceea0
heltonsmith777 <heltonsmith@gmail.com>
2023-02-05 03:57:41 -0300

    Ejercicio 11. Creación de rama hotfix-1 a partir de main y añadir un atributo lorem en clase A #12

--- ClaseA.java
+++ ClaseA.java
@@ -2,10 +2,12 @@ public class ClaseA{
 
     private String foo;
     private String bar;
+    private String lorem;
 
-    public ClaseA(String foo, String bar){
+    public ClaseA(String foo, String bar, String lorem){
         this.foo = foo;
         this.bar = bar;
+        this.lorem = lorem;
     }
 
     public String getFoo() {
@@ -16,12 +18,20 @@ public class ClaseA{
         return bar;
     }
 
+    public String getLorem() {
+        return lorem;
+    }
+
     public void setFoo(String foo) {
         this.foo = foo;
     }
 
     public void setBar(String bar) {
         this.bar = bar;
+    }
+
+    public void setLorem(String lorem) {
+        this.lorem = lorem;
     }
 
 }
